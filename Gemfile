0d1160297a7251380fe1ddc74adf7e311f29ea0a
Pedro Costa Neves <pedro@persocon.org>
2014-02-07 23:41:06 -0200

    slugfy method done with check up and etc

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ source 'https://rubygems.org'
 gem 'rake'
 
 # Component requirements
+gem 'stringex'
 gem 'carrierwave'
 gem 'carrierwave-sequel', :require => 'carrierwave/sequel'
 gem 'mini_magick'
--- models/content.rb
+++ models/content.rb
@@ -5,4 +5,32 @@ class Content < Sequel::Model
 	many_to_many :tags
 	many_to_many :uploads
 	many_to_many :categories
+	
+	# Callbacks
+	def before_save
+		slugfy
+	end
+	
+	private
+	
+	def slugfy
+		#if slug was typed do slugfy
+		unless slug.nil? || slug.blank?
+			self.slug = find_slugs slug.to_url
+			#if there's no slug typed try to get the title
+		else title.present?
+				self.slug = find_slugs title.to_url
+		end
+	end
+	
+	def find_slugs(slug)
+		num_of_slugs = Content.select(:slug, :id).where(:slug => slug).exclude(:id => id).all.count.to_int
+		#check if num_of_slugs returned something
+		if num_of_slugs > 0
+			return "#{slug}-#{num_of_slugs}"
+		else
+			return slug
+		end
+	end
+	
 end
