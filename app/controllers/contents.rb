07620a1902264c9c6c6e8ce7b63ea288608de62e
Pedro Barbosa Costa Neves <pbcneves@sp.r7.com>
2014-05-16 16:33:21 -0300

    starting to build a nice pagination for content :p

--- app/controllers/contents.rb
+++ app/controllers/contents.rb
@@ -1,6 +1,11 @@
 PhotoCms::App.controllers :contents, :map => '/api/v1/sets' do
-	get :index, :provides => [:html, :json] do
+	get :index do
+		"render backbone app here"
+	end
+
+	get :all, :provides => [:html, :json] do
 		callback = params.delete('callback') # jsonp
+
 		@contents = ContentHelper.get_all_json
 
 		if callback
