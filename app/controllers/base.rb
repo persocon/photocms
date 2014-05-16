abeaa867418487f5e01ab2b3e272d655e02bcc9e
Pedro Barbosa Costa Neves <pbcneves@sp.r7.com>
2014-05-16 16:54:48 -0300

    added a proper index, start to prepar pagination

---
+++ app/controllers/base.rb
@@ -0,0 +1,5 @@
+PhotoCms::App.controllers :base do
+  get :index, :map => "/" do
+    "BACKBONE"
+  end
+end
--- app/controllers/contents.rb
+++ app/controllers/contents.rb
@@ -1,11 +1,7 @@
 PhotoCms::App.controllers :contents, :map => '/api/v1/sets' do
-	get :index do
-		"render backbone app here"
-	end
-
-	get :all, :provides => [:html, :json] do
+	get :index, :provides => [:html, :json] do
 		callback = params.delete('callback') # jsonp
-
+		#params[:paged]
 		@contents = ContentHelper.get_all_json
 
 		if callback
@@ -14,6 +10,20 @@ PhotoCms::App.controllers :contents, :map => '/api/v1/sets' do
 	    else
 	      content_type :json
 	      response = @contents
+	    end
+	    response
+	end
+
+	get :page do
+		# "teste #{params}"
+		callback = params.delete('callback')
+
+		if callback
+	      content_type :js
+	      response = "#{callback}(#{params.to_json})" 
+	    else
+	      content_type :json
+	      response = params.to_json
 	    end
 	    response
 	end
