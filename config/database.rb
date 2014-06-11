fdec048087d43ca39ec9d5d667b3c41073ba22be
Pedro Barbosa Costa Neves <pbcneves@sp.r7.com>
2014-06-11 11:29:50 -0300

    Maroto

--- config/database.rb
+++ config/database.rb
@@ -2,6 +2,6 @@ Sequel::Model.plugin(:schema)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
   when :development then Sequel.connect("mysql2://root:root@localhost/photo_cms_development", :loggers => [logger])
-  when :production  then Sequel.connect("mysql2://rails:POfLUE54RB@localhost/photo_cms_production", :loggers => [logger])
+  when :production  then Sequel.connect("mysql2://persocon:accela503@localhost/photo_cms_production", :loggers => [logger])
   when :test        then Sequel.connect("mysql2://localhost/photo_cms_test",        :loggers => [logger])
 end
