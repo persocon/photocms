4fa988b748c3b8c241d85bfff72e75e76723fae5
Pedro Costa Neves <pedro@persocon.org>
2014-07-05 17:53:18 -0300

    loading maroto

--- public/javascripts/App/common/views.js
+++ public/javascripts/App/common/views.js
@@ -4,7 +4,39 @@ App.module("Common.Views", function(Views, App, Backbone, Marionette, $, _){
 	});
 	
 	Views.Loading = Marionette.ItemView.extend({
-		template: "#loading-view"
+		template: "#loading-view",
+		initialize: function(options){
+			var options = options || {};
+			this.title = options.title || "Loading Data";
+			this.message = options.message || "Please wait, data is loading.";
+		},
+		serializeData: function(){
+			return {
+				title: this.title,
+				message: this.message
+			}
+		},
+		onShow: function(){
+			var opts = {
+				lines: 13,
+				length: 20,
+				width: 10,
+				radius: 30,
+				corners: 1,
+				rotate: 0,
+				direction: 1,
+				color: "#FFF",
+				speed: 1,
+				trail: 50,
+				shadow: false,
+				hwaccel: true,
+				className: "spinner",
+				zIndex: 2e9,
+				top: "30px",
+				left: "auto"
+			};
+			$("#spinner").spin(opts);
+		}
 	});
 
 	Views.Layout = Marionette.Layout.extend({
